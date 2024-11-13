88e1f32ecf94a098f533d5e0f4ba4211a79a89f6
CMU02 <Hyeonjun1968@naver.com>
2024-11-13 18:20:50 +0900

    FIX : base잡기

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,31 @@
 import './App.css';
+import './styles/global.css';
+import './styles/Header.css'
 
 function App() {
-  // const [selectedCategory, setSelectedCategory] = useState(null); 
   return (
-<<<<<<< HEAD
     <>
-      <h1>Hello world</h1>
-    </>
-  )
-=======
-    <div className="app-container">
-      {/* 제목과 우측 텍스트 */}
-      <div className="header-right">
-          <p className="placeholder-text">서비스 소개 글</p> 
-      </div>
-      <div className="header-left">
-        <h1 className="project-title">요약좀</h1>
-      </div>
-      {/* 검색창과 카테고리 */}
       <header>
-        <input type="text" placeholder="검색하기" className="search-bar" />
-        <div className="categories">
+        <div className='title'>
+          <h1>요약좀</h1>
+        </div>
+        <div className='search-container'>
+          <div className='search-title'>
+            <p>검색하기</p>
+          </div>
+          <div className='search-bar'>
+            <input type="text" placeholder="검색하기"/>
+          </div>
+          <div className='search-btn'>
+            <img src='/images/search_icon.svg' alt='검색버튼'/>
+          </div>
+        </div>
+        <div className='info'>
+          <p>정보 과부하 속에서 핵심 내용을 빠르게 파악하기 어려운 시대입니다. &apos;3줄요약&apos; 서비스는 이러한 문제를 해결하기 위해 설계된 솔루션으로, 방대한 글이나 자료를 3줄로 간략하게 요약하여 사용자들이 중요한 정보를 놓치지 않고 효율적으로 습득할 수 있도록 돕습니다.</p> 
         </div>
       </header>
-    </div>
+    </>
   );
->>>>>>> 2d169ab (fix original code and add new files have to clean code)
 }
 
 export default App;
