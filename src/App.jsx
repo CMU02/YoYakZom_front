85c582723e8ad232a88b09adfbf155f3512089bd
CMU02 <Hyeonjun1968@naver.com>
2024-11-13 19:01:39 +0900

    - FIX : Header CSS

--- src/App.jsx
+++ src/App.jsx
@@ -15,9 +15,9 @@ function App() {
           </div>
           <div className='search-bar'>
             <input type="text" placeholder="검색하기"/>
-          </div>
-          <div className='search-btn'>
-            <img src='/images/search_icon.svg' alt='검색버튼'/>
+            <div className='search-btn'>
+              <img src='/images/search_icon.svg' alt='검색버튼'/>
+            </div>
           </div>
         </div>
         <div className='info'>
