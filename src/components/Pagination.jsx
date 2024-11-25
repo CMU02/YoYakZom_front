b026760ae39c5fdc947adfb331c60fd05e103318
HyeonJun <hyeonjun1968@naver.com>
2024-11-25 21:54:00 +0900

    fix : git 해결

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,54 +1,33 @@
-import React from 'react';
-import '../styles/Pagination.css';
+import '../styles/pagination.css';
 
-function Pagination({ total, pageSize, currentPage, onPageChange }) {
-  const totalPages = Math.ceil(total / pageSize);  // 총 페이지 수 계산
-
-  const handlePageClick = (pageNumber) => {
-    onPageChange(pageNumber);  // 페이지 클릭 시 페이지 변경
-  };
-
-  return (
-    <div className="pagination-container"> 
-      <button
-        className={`pagination-button ${currentPage === 1 ? 'disabled' : ''}`} 
-        disabled={currentPage === 1}
-        onClick={() => handlePageClick(1)}
-      >
-        처음
-      </button>
-      <button
-        className={`pagination-button ${currentPage === 1 ? 'disabled' : ''}`} 
-        disabled={currentPage === 1}
-        onClick={() => handlePageClick(currentPage - 1)}
-      >
-        이전
-      </button>
-      {[...Array(totalPages)].map((_, index) => (
-        <button
-          key={index}
-          className={`pagination-button ${currentPage === index + 1 ? 'active' : ''}`}
-          onClick={() => handlePageClick(index + 1)}
-        >
-          {index + 1}
-        </button>
-      ))}
-      <button
-        className={`pagination-button ${currentPage === totalPages ? 'disabled' : ''}`} 
-        disabled={currentPage === totalPages}
-        onClick={() => handlePageClick(currentPage + 1)}
-      >
-        다음
-      </button>
-      <button
-        className={`pagination-button ${currentPage === totalPages ? 'disabled' : ''}`} 
-        disabled={currentPage === totalPages}
-        onClick={() => handlePageClick(totalPages)}
-      >
-        마지막
-      </button>
-    </div>
-  );
+export default function Pagination({ currentPage, pageSize, handlePageChange }) {
+    return (
+        <div className='pagination'>
+            <button
+                onClick={() => handlePageChange(currentPage - 1)}
+                disabled={currentPage === 1}
+                className='prev-btn'
+            >
+                이전
+            </button>
+            {
+                Array.from({ length: pageSize }, (_, index) => index + 1).map(page => (
+                    <button
+                        onClick={() => handlePageChange(page)}
+                        key={page}
+                        className={currentPage === page ? 'number-active' : 'number'}
+                    >
+                        {page}
+                    </button>
+                ))
+            }
+            <button
+                onClick={() => handlePageChange(currentPage + 1)}
+                disabled={currentPage === pageSize}
+                className='next-btn'
+            >
+                다음
+            </button>
+        </div>
+    )
 }
-
-export default Pagination;
